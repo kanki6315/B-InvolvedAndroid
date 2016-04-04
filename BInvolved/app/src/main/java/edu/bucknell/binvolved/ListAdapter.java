cb909c5b66c54c622eaa5005129f09ad860be7b3
Henry <hbk001@bucknell.edu>
2016-04-04 09:46:52 -0400

    Initial commit of Organization list page

---
+++ BInvolved/app/src/main/java/edu/bucknell/binvolved/ListAdapter.java
@@ -0,0 +1,56 @@
+package edu.bucknell.binvolved;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.app.Activity;
+import java.util.list;
+
+public class ListAdapter extends ArrayAdapter {
+
+    private Context context;
+    private boolean useList = true;
+
+    public ListAdapter(Context context, List items){
+        super(context. android.R.layout.simple_list_item_1, items);
+        this.context = context;
+    }
+
+    private class ViewHolder{
+        TextView titleText;
+    }
+
+    public View getView(int pos, View convertView, ViewGroup parent){
+        ViewHolder holder = null;
+        ListItem item = (ListItem)getItem(position);
+        View viewToUse = null;
+
+        // This block exists to inflate the settings list item conditionally based on whether
+        // we want to support a grid or list view.
+        LayoutInflater mInflater = (LayoutInflater) context
+                .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null) {
+            if(useList){
+                viewToUse = mInflater.inflate(R.layout.example_list_item, null);
+            } else {
+                viewToUse = mInflater.inflate(R.layout.example_grid_item, null);
+            }
+
+            holder = new ViewHolder();
+            holder.titleText = (TextView)viewToUse.findViewById(R.id.titleTextView);
+            viewToUse.setTag(holder);
+        } else {
+            viewToUse = convertView;
+            holder = (ViewHolder) viewToUse.getTag();
+        }
+
+        holder.titleText.setText(item.getItemTitle());
+        return viewToUse;
+    }
+}
+    }
+}
+
---
+++ BInvolved/app/src/main/java/edu/bucknell/binvolved/ListItem.java
@@ -0,0 +1,18 @@
+package edu.bucknell.binvolved;
+
+public class ListItem {
+
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public ListItem(String title){
+        this.title = title;
+    }
+}
