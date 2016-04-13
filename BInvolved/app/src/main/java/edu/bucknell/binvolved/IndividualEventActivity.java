666c48ba3f1923b48e627a67a670f4a61e7fff1d
Arjuna Kankipati <kanki6315@gmail.com>
2016-04-13 19:58:54 -0400

    Added changes for individual event

--- BInvolved/app/src/main/java/edu/bucknell/binvolved/IndividualEventActivity.java
+++ BInvolved/app/src/main/java/edu/bucknell/binvolved/IndividualEventActivity.java
@@ -1,8 +1,10 @@
 package edu.bucknell.binvolved;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.Toolbar;
+        import android.support.v7.app.AppCompatActivity;
+        import android.support.v7.widget.Toolbar;
+        import android.os.Bundle;
+        import android.view.Menu;
+        import android.view.MenuItem;
 
 public class IndividualEventActivity extends AppCompatActivity {
 
@@ -11,6 +13,28 @@ public class IndividualEventActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_individual_event);
 
-        //getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_individual_event, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_search) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
