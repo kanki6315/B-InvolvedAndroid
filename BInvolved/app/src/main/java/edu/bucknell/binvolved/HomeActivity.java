dacc675391c1d6c2a09c4ab2ca00198be1ad9668
Arjuna Kankipati <kanki6315@gmail.com>
2016-04-21 16:21:12 -0400

    Minor changes to home screen

--- BInvolved/app/src/main/java/edu/bucknell/binvolved/HomeActivity.java
+++ BInvolved/app/src/main/java/edu/bucknell/binvolved/HomeActivity.java
@@ -59,16 +59,7 @@ public class HomeActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
+        getSupportActionBar().setTitle("Home");
 
         // read in the CSV files
         readInOrganizations();
